963145ec8b33613f2b914b85dd4bcbac192ef4f9
Itsolution-git <roy.smith0820@gmail.com>
2018-12-05 04:24:23 -0500

    Update for commonbrain domain

--- client/src/shared/constants.js
+++ client/src/shared/constants.js
@@ -4,7 +4,9 @@ if (window.location.hostname.indexOf('localhost') != -1)
 if (window.location.hostname.indexOf('18.222.107.103') != -1)
 	url = 'http://18.222.107.103:3000'
 if (window.location.hostname.indexOf('18.191.231.106') != -1)
-	url = 'http://18.191.231.106:3000'
+	url = 'http://18.191.231.106'
+if (window.location.hostname.indexOf('commonbrain') != -1)
+	url = 'http://commonbrain.io'
 export default {
   API_BASE_URL: url
 }
